92d1f0fbc352aec5a54f8ebc4bf684238aa30074
DawidKaszubaCT <144652698+DawidKaszubaCT@users.noreply.github.com>
2023-11-07 10:27:02 +0100

    Add files via upload

---
+++ PracaTakaO2/PracaTakaO2.cpp
@@ -0,0 +1,39 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    //Zapis i tworzenie pliku
+    //--------------------------------------------------------------------------------------
+    fstream plik;
+    char tresc[100];
+    plik.open("artykul.txt", ios::in | ios::out | ios::trunc);
+    cout << "Wprowadz artykul: " << endl;
+    cin.getline(tresc, 100, '\n');
+
+    plik << tresc;
+
+    plik.close();
+    //-------------------------------------------------------------------------------------
+    //wczytywanie danych z pliku artykul.txt
+
+    ifstream plikOdczyt;
+
+    plikOdczyt.open("artykul.txt");
+
+    if (plikOdczyt.is_open()) {
+        string linia, zawartosc;
+            char znak;
+        while (!plikOdczyt.eof())
+        {
+            znak = plikOdczyt.get();
+            if (plikOdczyt.good()) cout << znak << endl;
+        }
+    }
+
+    system("pause");
+
+    EXIT_SUCCESS;
+}
---
+++ PracaTakaO3/PracaTakaO3.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    //Wczytywanie zkonkretnych znaków z pliku od znaku X do Y
+    fstream plik;
+    char tresc[100];
+    plik.open("artykul.txt", ios::out);
+
+    if (plik.good()) {
+        plik.seekg(9);
+        //plik.read(tresc, 4);
+    }
+    else {
+        cout << "Plik nie istnieje" << endl;
+    }
+    plik.close();
+
+    system("pause");
+
+    EXIT_SUCCESS;
+}
