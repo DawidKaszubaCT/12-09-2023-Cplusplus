797c6f03b06d847113a4eb2ddb1c93958e08afc9
DawidKaszubaCT <144652698+DawidKaszubaCT@users.noreply.github.com>
2023-11-28 10:32:44 +0100

    Add files via upload

---
+++ Tablice/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+int main()
+{
+  /*  setlocale(LC_ALL, "");
+    int* tablica;
+    int ile = 5;
+    tablica = new int[ile];
+
+    cout << "Dynamiczne Adresy do przydzielania 5 ale = " << (int)tablica << endl;
+    cout << "Adres własny wskaznika tablica = " << (int)&tablica << endl;
+    cout << (int)&tablica[0] << endl;
+    cout << (int)&tablica[1] << endl;
+    cout << (int)&tablica[2] << endl;
+    cout << (int)&tablica[3] << endl;
+    cout << (int)&tablica[4] << endl;
+
+    // inny sposób wyświetlenia adresów z tablicy
+    cout << endl << endl;
+    for (int i = 0; i < ile; i++)
+    {
+        cout << (int)tablica << endl;
+        tablica++;
+    }
+    //--------------------------------------------
+    return 0;*/
+    //Napisz app na konsole która losuje określoną ilość liczb i przypisuje do dynamicznej tablicy. Wyznacz sumę liczb średnią liczb
+    //1.Podaj ile elementów losujemyy : 10
+    //2. Wylosuj z zakresu od 0 do 100 10 liczb
+    //3. Wyznacz sumę liczb i średnią liczb
+
+
+    srand(time(NULL))
+    return EXIT_SUCCESS;
+}
---
+++ Tablice2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    clock_t start, stop;
+
+
+    start = clock();
+
+    cout << "Dynamiczne przydzielanie pamieci dla tablic - WSKAZNIKI" << endl;
+    int rozmiarTablicy;
+    int los;
+    cout << "Podaj rozmiar tablicy = "; cin >> rozmiarTablicy;
+    int* tablica;
+    tablica = new int[rozmiarTablicy];
+
+    for (int i = 0; i < rozmiarTablicy; i++) {
+        los = rand() % 100 + 1;
+        tablica[i] = los;
+    }
+
+    for (int i = 0; i < rozmiarTablicy; i++)
+    {
+        cout << "Index nr " << i << " = " << tablica[i] << " Adres komorki: " << (int)&tablica[i] << endl;
+    }
+
+    stop = clock();
+
+    double czaswyswietlaniaAplikacji = (double)(stop - start) / CLOCKS_PER_SEC;
+
+    cout << "Czas wyswietlania:" << czaswyswietlaniaAplikacji << endl;
+
+    return EXIT_SUCCESS;
+    //napisz program na konsole który przydziela(losuje) z zakresu od 0 do 100 wartości znaków z tablicy asci, znaki przypisz do tablicy oraz je wyświetl
+}
