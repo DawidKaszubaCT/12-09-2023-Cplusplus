5bc444daa81307bc175764257f42277f4e397c6b
DawidKaszubaCT <144652698+DawidKaszubaCT@users.noreply.github.com>
2023-12-05 10:29:45 +0100

    Add files via upload

---
+++ 05.12.2023/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+#include <iostream>
+#include <time.h>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    vector <int> tablica;
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        int los = rand() % 100 + 1;
+        tablica.push_back(los);
+    }
+    sort(tablica.begin(), tablica.end());
+    for (size_t i = 0; i < tablica.size(); i++)
+    {
+        cout << tablica[i] << endl;
+    }
+    return EXIT_SUCCESS;
+}
