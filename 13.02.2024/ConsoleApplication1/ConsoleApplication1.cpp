f4c0d15035ce5ef52003829f1d76fc06ce9597f8
DawidKaszubaCT <144652698+DawidKaszubaCT@users.noreply.github.com>
2024-02-13 10:38:18 +0100

    Add files via upload

---
+++ 13.02.2024/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,92 @@
+﻿/*#include <iostream>
+using namespace std;
+
+class Robot {
+public:
+    string name;
+    int serial_number;
+    int digit;
+
+    void runRobot() {
+        if( digit == 1){
+            showRobot();
+        }
+        else {
+            messages();
+        }
+    }
+    void showRobot()
+    {
+        cout << "Robot uruchomiony: " << serial_number << "Nazwa: " << name << endl;
+    }
+    void messages()
+    {
+        cout << "Robot nieuruchomiony" << endl;
+    }
+};
+
+
+int main()
+{
+    int uruchomienie;
+    cout << "Robot 1 - Start, inna wartość - Robot nie działa" << endl;
+    cout << "Co chcesz zrobić - "; cin >> uruchomienie;
+    cout << "---------------------------------------------" << endl;
+    Robot mojrobocik;
+    mojrobocik.digit = uruchomienie;
+    mojrobocik.serial_number = 23456798765;
+    mojrobocik.name = "Roomba";
+    mojrobocik.runRobot();
+}
+*/
+#include <iostream>
+using namespace std;
+
+class Machine {
+public:
+    string name;
+    string co_robi;
+    string isXbox;
+    int digit;
+
+    void runMachine() {
+        showMachine();
+    }
+    void showMachine()
+    {
+        cout << "Maszyna uruchomiona bracie B) Nazwa: " << name << "Co robi: " << co_robi << endl;
+    }
+    void messages()
+    {
+        cout << "Maszyna nieuruchomiona" << endl;
+    }
+    void checkXbox()
+    {
+        if (isXbox == "tak")
+        {
+            cout << "Sigma xbox bratku" << endl;
+        }
+    }
+};
+
+
+int main()
+{
+    Machine mojamachina;
+    int uruchomienie;
+    int xbox;
+    int uruchomienie2;
+    cout << "Machina symulator :) : 1 - Start, inna wartosc - Czarnobyl" << endl;
+    cout << "Co chcesz zrobic - "; cin >> uruchomienie;
+    if (uruchomienie != 1)
+    {
+        cout << "O ty rusku przeklety" << endl;
+        return EXIT_SUCCESS;
+    }
+    cout << "Co ona robi wogole? (bez spacji pls): "; cin >> mojamachina.co_robi;
+    cout << "Xbox? - wproadz tak jeśli....tak: : "; cin >> xbox;
+    cout << "---------------------------------------------" << endl;
+    mojamachina.runMachine();
+    mojamachina.checkXbox();
+   
+}
---
+++ 13.02.2024/Kalkulakukalkulakukalkulaku/Kalkulakukalkulakukalkulaku.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+class Dodawanie {
+public:
+    int liczba1;
+    int liczba2;
+    int oblicz() {
+        return this->liczba1 + this->liczba2;
+    };
+};
+class Odejmowanie {
+public:
+    int liczba1;
+    int liczba2;
+    int oblicz() {
+        return this->liczba1 - this->liczba2;
+    };
+};
+class Mnożenie {
+public:
+    int liczba1;
+    int liczba2;
+    int oblicz() {
+        return this->liczba1 * this->liczba2;
+    };
+};
+int main()
+{
+    int a, b;
+    setlocale(LC_ALL, "");
+    cout << "Podaj a = "; cin >> a;
+    cout << "Podaj b = "; cin >> b;
+
+    Dodawanie dodawanie;
+    dodawanie.liczba1 = a;
+    dodawanie.liczba2 = b;
+    cout << "Wynik dodawania = " << dodawanie.oblicz() << endl;
+    Odejmowanie odejmowanie;
+    odejmowanie.liczba1 = a;
+    odejmowanie.liczba2 = b;
+    cout << "Wynik odejmowanie = " << odejmowanie.oblicz() << endl;
+    Mnożenie mnozenie;
+    mnozenie.liczba1 = a;
+    mnozenie.liczba2 = b;
+    cout << "Wynik mnozenie = " << mnozenie.oblicz() << endl;
+    return EXIT_SUCCESS;
+}
