7bd970295ad10c7b23f06d527f3c28468e5e1afd
DawidKaszubaCT <144652698+DawidKaszubaCT@users.noreply.github.com>
2024-03-12 10:33:25 +0100

    Add files via upload

---
+++ 12.03.24/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+class Pojazdy {
+private:
+    string name;
+public:
+    Pojazdy(string name) {
+        this->name = name;
+    }
+    void prezentacjaPojazdu() {
+        cout << "Prezentacja pojazdu; " << this->name << endl;
+    }
+};
+
+class Samochody : public Pojazdy {
+private:
+    string marka;
+    string nazwa;
+public:
+    Samochody(string m, string n):Pojazdy(n) {
+        this->marka = m;
+        this->nazwa = n;
+    }
+    void prezentacjaSamochodu() {
+        cout << "Prezentacja samochodu" << this->marka << endl;
+        cout << "nazwa pojazdu" << this->nazwa << endl;
+    }
+};
+int main()
+{
+    Samochody vw = Samochody("VW", "PASSAT");
+
+    vw.prezentacjaPojazdu();
+    vw.prezentacjaSamochodu();
+    
+
+
+    return EXIT_SUCCESS;
+}
